9e96bd3b8858d539ce005f0980ff57e6f7fb951d
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-11-03 12:58:16 +0100

    vito's update

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 //codice sconto
 const discountCode = "SERVLET20";
 
-//alert("benvenuto, questo è il codice sconto: " + discountCode);
+alert("benvenuto, questo è il codice sconto: " + discountCode);
 
 //NOME BURGER DA PROMPT
-//var burgerName = prompt("Inserisci il nome del burger");
+var burgerName = prompt("Inserisci il nome del burger");
 
+var costo = 4.50
 //per stampare il nome del burger in HTML
-//document.getElementById("hamburger-name").textContent = "The " + burgerName + " Burger";
+if(burgerName === ""){
+    document.getElementById("hamburger-name").textContent = "The Valerio Burger";
+}else if(burgerName === "Simone"){
+    document.getElementById("hamburger-name").textContent = "The " + burgerName + " Burger";
+    costo = 6.99
+}else{
+    document.getElementById("hamburger-name").textContent = "The " + burgerName + " Burger";
+}
+
 
 //ref per calcolo
 var bottoneCalcolo = document.getElementById("calcoloTotale");
@@ -38,10 +47,23 @@ console.log(ingr1.value);
 
 // listener bottone
 bottoneCalcolo.addEventListener("click", function() {
+
+    let arrayIng = [ingr1,ingr2, ingr3, ingr4, ingr5 , ingr6 ];
+
+    for (let i=0; i<arrayIng.length; i++) {
+        if (arrayIng[i].checked) {
+            costo += parseFloat(arrayIng[i].value);
+        }
+    }
+
+    if(input.value.toUpperCase() === discountCode){
+        costo = costo - (costo * 20)/100;
+    }
     
     
-    console.log(ingr1.checked);
-    console.log(prezzoBurger);
+    //console.log(ingr1.checked);
+    //console.log(prezzoBurger);
+    document.getElementById("price").innerHTML = costo.toFixed(2) + "€";
 })
 
 
